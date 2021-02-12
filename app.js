3eed83ade64aba1545eca9d54c92191e29d07126
Abhishek <Abhisheky418@gmail.com>
2021-02-12 23:05:54 +0530

    added sql database connectivity

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const errorController = require('./controllers/errorController');
+const db = require('./util/database');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,6 +12,10 @@ app.set('views', 'views');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
+
+db.execute('SELECT * FROM products').then(result => {
+  console.log(result[0]);
+}).catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
